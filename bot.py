eee32f66dc6dc57cf5bf779fdd59f77ed60304b0
Rendy Arya Kemal <rendyarya22@gmail.com>
2019-10-21 17:30:40 +0700

    User mention should be optional

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from discord.ext import commands
+from typing import Optional
 import discord
 import config
 from osuapi import APIWrapper, get_username, get_mapset_ids
@@ -24,16 +25,19 @@ bot = RenBot()
 # write general commands here
 
 @bot.command(aliases=["v", "verif"])
-async def verify(ctx, user : discord.Member, profile_url):
+async def verify(ctx, profile_url : str, *, user = None):
     if not user:
-        await ctx.send("Please mention user!")
+        user = ctx.author
+
+    if not isinstance(user, discord.Member):
+        ctx.send("Please send valid member as second argument!")
         return
+
     if not profile_url:
         await ctx.send("Please provide osu! profile link!")
         return
     
     osuUser = await APIWrapper(get_username(profile_url))
-    
     if not osuUser:
         await ctx.send("Cannot find any user with that url, are you restricted?")
         return
